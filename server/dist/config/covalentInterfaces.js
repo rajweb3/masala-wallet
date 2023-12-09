7d1f5b5b0e5643161ef7d2bcd104a2a80f34946e
Prince Raj <rajprince.cs@gmail.com>
2023-12-09 20:11:41 +0000

    removed build files

--- server/dist/config/covalentInterfaces.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCovalentChainName = void 0;
-const getCovalentChainName = (covalentChain) => {
-    return covalentChain;
-};
-exports.getCovalentChainName = getCovalentChainName;
