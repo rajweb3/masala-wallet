64e78b63f49fa8d2f6a99eeee39f85cbd9b74931
aditya-vijaykumar <aditya.tarus@gmail.com>
2023-12-09 18:04:09 +0530

    Hardhat Setup

---
+++ contract/scripts/deploy.ts
@@ -0,0 +1,13 @@
+import { formatEther, parseEther } from "viem";
+import hre from "hardhat";
+
+async function main() {
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
